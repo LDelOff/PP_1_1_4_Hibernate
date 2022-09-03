fe699e26c9cef7c718df7ea5cfad93a25c679f35
L_DelOff <51275636+LDelOff@users.noreply.github.com>
2022-09-03 15:38:10 +0300

    Переделал для Hibernate

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,28 +1,44 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
 
 public class Util {
     // Тут настройки подключения
     public static final String url = "jdbc:mysql://localhost:3306/pp_1_1_3-4_jdbc_hibernate";
     public static final String user = "root";
     public static final String pass = "3Iehegf@be";
-    public static Connection connection2database; // Это будет объект коннекшн
 
-    public static Connection getConnection() {
-        // попытались инициализировать драйвер
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getConnection() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            Configuration configuration = new Configuration();
+
+            Properties properties = new Properties();
+            properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            properties.put(Environment.URL, url);
+            properties.put(Environment.USER, user);
+            properties.put(Environment.PASS, pass);
+            properties.put(Environment.SHOW_SQL, false);
+            properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+            configuration.setProperties(properties);
+            configuration.addAnnotatedClass(User.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
-        try {
-            connection2database = DriverManager.getConnection(url,user,pass); // Тут я подключился к БД
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return connection2database;
+        return sessionFactory;
     }
 }
