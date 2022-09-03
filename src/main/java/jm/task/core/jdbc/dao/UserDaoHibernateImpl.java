09e78bd7d4d9a42300a26340381c1de3224ec3cc
L_DelOff <51275636+LDelOff@users.noreply.github.com>
2022-09-03 15:38:43 +0300

    Реализация методов для Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,95 @@
 package jm.task.core.jdbc.dao;
 
+
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+    private static final SessionFactory sessionFactory = Util.getConnection();
+
     public UserDaoHibernateImpl() {
 
     }
 
-
+    //Методы создания таблицы пользователей в классе UserHibernateDaoImpl должны быть реализованы с помощью SQL.
     @Override
     public void createUsersTable() {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS Persons (\n"
+                    + " `id` INT NOT NULL AUTO_INCREMENT,\n"
+                    + " `name` VARCHAR(50) NOT NULL,\n"
+                    + " `lastName` VARCHAR(50) NOT NULL,\n"
+                    + " `age` INT NOT NULL,\n"
+                    + " PRIMARY KEY (`id`),\n"
+                    + " UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
+    //Методы удаления таблицы пользователей в классе UserHibernateDaoImpl должны быть реализованы с помощью SQL.
     @Override
     public void dropUsersTable() {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS Persons").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = sessionFactory.openSession()) {
+            User user = new User(name, lastName, age);
+            Transaction transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.delete(session.get(User.class, id));
+            transaction.commit();
+        } catch (HibernateException exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = null;
+        try (Session session = sessionFactory.openSession()) {
+            userList = session.createQuery("SELECT a FROM User a", User.class).getResultList();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createSQLQuery("delete from Persons").executeUpdate();
+            transaction.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
 
     }
 }
