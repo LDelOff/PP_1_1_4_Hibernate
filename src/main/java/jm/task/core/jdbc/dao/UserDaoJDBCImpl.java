56559d403ab824ac28bd298c4e787b32fed3db0d
L_DelOff <51275636+LDelOff@users.noreply.github.com>
2022-09-03 15:26:35 +0300

    Пробовал исключить JDBC имплементацию

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,75 +8,76 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private Connection connection = Util.getConnection(); // Соединимся с БД
+    //private Connection connection = Util.getConnection(); // Соединимся с БД
 
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS `Persons` (\n"
-                    + " `id` INT  PRIMARY KEY AUTO_INCREMENT,\n"
-                    + " `name` VARCHAR(50) NOT NULL,\n"
-                    + " `lastName` VARCHAR(50) NOT NULL,\n"
-                    + " `age` INT NOT NULL);");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+//        try (Statement statement = connection.createStatement()) {
+//            statement.executeUpdate("CREATE TABLE IF NOT EXISTS `Persons` (\n"
+//                    + " `id` INT  PRIMARY KEY AUTO_INCREMENT,\n"
+//                    + " `name` VARCHAR(50) NOT NULL,\n"
+//                    + " `lastName` VARCHAR(50) NOT NULL,\n"
+//                    + " `age` INT NOT NULL);");
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public void dropUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("DROP TABLE IF EXISTS `Persons`");
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-        }
+//        try (Statement statement = connection.createStatement()) {
+//            statement.executeUpdate("DROP TABLE IF EXISTS `Persons`");
+//        } catch (SQLException exception) {
+//            exception.printStackTrace();
+//        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO `Persons`"
-                + "(name, lastName, age) VALUES (?, ?, ?)")) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-        } catch (SQLException | NullPointerException exception) {
-            exception.printStackTrace();
-        }
+//        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO `Persons`"
+//                + "(name, lastName, age) VALUES (?, ?, ?)")) {
+//            preparedStatement.setString(1, name);
+//            preparedStatement.setString(2, lastName);
+//            preparedStatement.setByte(3, age);
+//            preparedStatement.executeUpdate();
+//        } catch (SQLException | NullPointerException exception) {
+//            exception.printStackTrace();
+//        }
     }
 
     public void removeUserById(long id) {
-        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM `Persons`  WHERE id = ?")) {
-            preparedStatement.setLong(1, id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-        }
+//        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM `Persons`  WHERE id = ?")) {
+//            preparedStatement.setLong(1, id);
+//            preparedStatement.executeUpdate();
+//        } catch (SQLException exception) {
+//            exception.printStackTrace();
+//        }
     }
 
     public List<User> getAllUsers() {
-        List<User> listUser = new ArrayList<>();
-        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery("SELECT * FROM `Persons`")) {
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong(1));
-                user.setName(resultSet.getString(2));
-                user.setLastName(resultSet.getString(3));
-                user.setAge(resultSet.getByte(4));
-                listUser.add(user);
-            }
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-        }
-        return listUser;
+//        List<User> listUser = new ArrayList<>();
+//        try (Statement statement = connection.createStatement(); ResultSet resultSet = statement.executeQuery("SELECT * FROM `Persons`")) {
+//            while (resultSet.next()) {
+//                User user = new User();
+//                user.setId(resultSet.getLong(1));
+//                user.setName(resultSet.getString(2));
+//                user.setLastName(resultSet.getString(3));
+//                user.setAge(resultSet.getByte(4));
+//                listUser.add(user);
+//            }
+//        } catch (SQLException exception) {
+//            exception.printStackTrace();
+//        }
+//        return listUser;
+        return null;
     }
 
     public void cleanUsersTable() {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("TRUNCATE TABLE `Persons`");
-        } catch (SQLException exception) {
-            exception.printStackTrace();
-        }
+//        try (Statement statement = connection.createStatement()) {
+//            statement.executeUpdate("TRUNCATE TABLE `Persons`");
+//        } catch (SQLException exception) {
+//            exception.printStackTrace();
+//        }
     }
 }
